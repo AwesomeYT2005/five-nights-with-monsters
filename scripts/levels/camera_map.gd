c5af5cbed7b5ef6d68711c6e796041815a5a5eac
EggyWorking <37382711+AwesomeYT2005@users.noreply.github.com>
2026-01-14 08:15:46 +0000

    Camera Map Working
    Complete Camera Map with working animations and button changes. Buttons not yet fully functional.
    Added optional smiling monster to Window.

--- scripts/levels/camera_map.gd
+++ scripts/levels/camera_map.gd
@@ -1,4 +1,21 @@
 extends Node2D
 
+@onready var cam_tree: AnimatedSprite2D = $CameraTree
+@onready var floor_1_buttons: Control = $Floor1Buttons
+@onready var floor_2_buttons: Control = $Floor2Buttons
+
+func _ready() -> void:
+	floor_1_buttons.visible = false
+
 func _on_floor_1_pressed() -> void:
-	print("This floor is not available at the moment")
+	cam_tree.animation = "floor_1"
+	floor_1_buttons.visible = true
+	floor_2_buttons.visible = false
+
+func _on_floor_2_pressed() -> void:
+	cam_tree.animation = "floor_2"
+	floor_1_buttons.visible = false
+	floor_2_buttons.visible = true
+
+func _on_front_hall_pressed() -> void:
+	pass

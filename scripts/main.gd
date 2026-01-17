b07e02ab81dffb932e504a299b2cd5b16aae5477
EggyWorking <37382711+AwesomeYT2005@users.noreply.github.com>
2026-01-17 09:37:53 +0000

    Chirrup Jumpscare
    Added a repeatable jumpscare system, however, only Chirrup jumpscares the player at the moment.

--- scripts/main.gd
+++ scripts/main.gd
@@ -3,18 +3,48 @@ extends Node
 @onready var fade: ColorRect = $BlackScreen
 @onready var death_sound: AudioStreamPlayer2D = $DeathSound
 
+#region Audio
+
 const DOOR_OPEN_CLOSE = preload("res://assets/sounds/enemies/doorman/door_open_close.mp3")
 const WINDOW_SMASH = preload("res://assets/sounds/enemies/chirrup/window-smash.mp3")
 const LAUGH_DEATH = preload("res://assets/sounds/enemies/chirrup/laugh-death.mp3")
 const TOYS_DYING = preload("res://assets/sounds/enemies/corruption/toys_dying.mp3")
+const MONSTER_ROAR = preload("res://assets/sounds/enemies/chirrup/monster-roar.mp3")
+
+#endregion
+
+#region Scenes
+
 var main_menu_scene = preload("res://scenes/menu/main_menu.tscn")
 var level_scene = preload("res://scenes/levels/level.tscn")
 
 var main_menu_instance
 var level_instance
 
+#endregion
+
+#region Enemy Jumpscares
+
+@onready var jumpscare_timer: Timer = $JumpscareTimer
+const chirrup_scare_scene = preload("res://scenes/enemies/chirrup.tscn")
+var jumpscare
+var jumpscared: bool = false
+var jumpscared_by
+
+#endregion
+
 func _ready() -> void:
 	load_main_menu()
+
+func _process(delta: float) -> void:
+	if jumpscared == true:
+		var j = get_node_or_null(jumpscared_by)
+		j.scale *= Vector2(delta+1.01,delta+1.01)
+		j.position += Vector2(randi_range(-3,3),randi_range(-3,3))
+		if j.scale > Vector2(45,45):
+			jumpscared = false
+			death_sound.stop()
+			death_sound.finished.emit()
 
 func _on_play_button_pressed() -> void:
 	level_instance = level_scene.instantiate()
@@ -34,23 +64,43 @@ func load_main_menu() -> void:
 	play_button.pressed.connect(_on_play_button_pressed)
 	quit_button.pressed.connect(_on_quit_button_pressed)
 
+func enemy_jumpscare(enemy) -> void:
+	if enemy == "Chirrup":
+		jumpscare = chirrup_scare_scene.instantiate()
+		self.add_child(jumpscare)
+		jumpscare.scale = Vector2(0.1,0.1)
+		jumpscare.position = Vector2(643,357)
+		jumpscared = true
+		death_sound.stream = MONSTER_ROAR
+		death_sound.play(0.75)
+
 func _on_camera_map_player_dead(enemy) -> void:
 	fade.visible = true
+	jumpscared_by = enemy
 	if enemy == "The Doorman":
 		death_sound.stream = DOOR_OPEN_CLOSE
 	if enemy == "Chirrup":
 		death_sound.stream = WINDOW_SMASH
 	if enemy == "The Corruption":
 		death_sound.stream = TOYS_DYING
-	var killtag = fade.get_node_or_null("TextureRect2/KillTag")
+	var killtag = fade.get_node_or_null("Labels/TextureRect2/KillTag")
 	killtag.text = enemy
 	death_sound.play(0.0)
+	jumpscare_timer.start()
 	level_instance.queue_free()
 
 func _on_death_sound_finished() -> void:
 	if death_sound.stream == WINDOW_SMASH:
+		pass
+	elif death_sound.stream == MONSTER_ROAR:
 		death_sound.stream = LAUGH_DEATH
-		death_sound.play()
+		death_sound.play(0.0)
+		get_node(jumpscared_by).queue_free()
+		fade.get_node("Labels").visible = true
 	else:
 		load_main_menu()
 		fade.visible = false
+		fade.get_node("Labels").visible = false
+
+func _on_jumpscare_timer_timeout() -> void:
+	enemy_jumpscare(jumpscared_by)
